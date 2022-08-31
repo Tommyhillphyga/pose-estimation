695ab83f1d8266f5e6d63e2ce2397d2002225319
unknown <tommyhillphyga@gmail.com>
2022-08-31 06:59:36 +0100

    updated

--- run_pose.py
+++ run_pose.py
@@ -3,20 +3,21 @@ import time
 import torch
 import argparse
 import numpy as np
-from torchvision import transforms
 from utils.datasets import letterbox
 from utils.torch_utils import select_device
 from models.experimental import attempt_load
 from utils.plots import output_to_keypoint, plot_skeleton_kpts
 from utils.general import non_max_suppression_kpt, strip_optimizer
+from torchvision import transforms
+
 
 
 @torch.no_grad()
-def run(poseweights=f'yolov7-w6-pose.pt', source='pose.mp4', device='cpu'):
+def run(poseweights= 'yolov7-w6-pose.pt', source='pose.mp4', device='cpu'):
 
     path = source
     ext = path.split('/')[-1].split('.')[-1].strip().lower()
-    if ext in ["mp4", "webm", "avi"] or ext.isnumeric():
+    if ext in ["mp4", "webm", "avi"] or ext not in ["mp4", "webm", "avi"] and ext.isnumeric():
         input_path = int(path) if path.isnumeric() else path
         device = select_device(opt.device)
         half = device.type != 'cpu'
@@ -34,24 +35,20 @@ def run(poseweights=f'yolov7-w6-pose.pt', source='pose.mp4', device='cpu'):
             cap.read()[1], (frame_width), stride=64, auto=True)[0]
         resize_height, resize_width = vid_write_image.shape[:2]
         out_video_name = "output" if path.isnumeric else f"{input_path.split('/')[-1].split('.')[0]}"
-        out = cv2.VideoWriter(f"{out_video_name}_result4.mp4",
-                              cv2.VideoWriter_fourcc(*'mp4v'), 30,
-                              (resize_width, resize_height))
+        out = cv2.VideoWriter(f"{out_video_name}_result4.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, (resize_width, resize_height))
 
         frame_count, total_fps = 0, 0
 
-        while(cap.isOpened):
+        while cap.isOpened:
 
-            print("Frame {} Processing".format(frame_count))
-
+            print(f"Frame {frame_count} Processing")
             ret, frame = cap.read()
             if ret:
                 orig_image = frame
 
                 # preprocess image
                 image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
-                image = letterbox(image, (frame_width),
-                                  stride=64, auto=True)[0]
+                image = letterbox(image, (frame_width), stride=64, auto=True)[0]
                 image_ = image.copy()
                 image = transforms.ToTensor()(image)
                 image = torch.tensor(np.array([image.numpy()]))
@@ -63,15 +60,16 @@ def run(poseweights=f'yolov7-w6-pose.pt', source='pose.mp4', device='cpu'):
                 with torch.no_grad():
                     output, _ = model(image)
 
-                output = non_max_suppression_kpt(
-                    output, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'], kpt_label=True)
+                output = non_max_suppression_kpt(output, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'], kpt_label=True)
                 output = output_to_keypoint(output)
                 img = image[0].permute(1, 2, 0) * 255
                 img = img.cpu().numpy().astype(np.uint8)
 
                 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
                 for idx in range(output.shape[0]):
                     plot_skeleton_kpts(img, output[idx, 7:].T, 3)
+
                 if ext.isnumeric():
                     cv2.imshow("Detection", img)
                     key = cv2.waitKey(1)
@@ -93,12 +91,9 @@ def run(poseweights=f'yolov7-w6-pose.pt', source='pose.mp4', device='cpu'):
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--poseweights', nargs='+', type=str,
-                        default='yolov7-w6-pose.pt', help='model path(s)')
-    parser.add_argument('--source', type=str,
-                        default='./yoga/yoga8.mp4', help='video/0 for webcam')
-    parser.add_argument('--device', type=str, default='cpu',
-                        help='cpu/0,1,2,3(gpu)')
+    parser.add_argument('--poseweights', nargs='+', type=str, default='yolov7-w6-pose.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, help='path to video or 0 for webcam')
+    parser.add_argument('--device', type=str, default='cpu', help='cpu/0,1,2,3(gpu)')
     opt = parser.parse_args()
     return opt
 
